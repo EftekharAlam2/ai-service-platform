1f9d0a424c2340cedad6cbe18f0237ac38a49076
EftekharAlam2 <hinsouvlmare@gmail.com>
2023-09-03 22:01:59 +0600

    image layout const

--- app/(dashboard)/(routes)/dashboard/page.tsx
+++ app/(dashboard)/(routes)/dashboard/page.tsx
@@ -15,10 +15,10 @@ export default function HomePage() {
     <div>
       <div className="mb-8 space-y-4">
         <h2 className="text-2xl md:text-4xl font-bold text-center">
-          Discover the AI Advantage
+          Explore the power of AI
         </h2>
         <p className="text-muted-foreground font-light text-sm md:text-lg text-center">
-          Interact with Intelligent AI - Discover AI&apos;s Potential
+          Chat with the smartest AI - Experience the power of AI
         </p>
       </div>
       <div className="px-4 md:px-20 lg:px-32 space-y-4">
